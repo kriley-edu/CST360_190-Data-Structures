82e00fa10c66fd9ce521e3c07c8b0f0b0cce16b1
kriley-edu <kriley97@gmail.com>
2026-05-11 13:36:49 -0400

    Update payroll.py

--- Chapter01/Ex6/Starter/payroll.py
+++ Chapter01/Ex6/Starter/payroll.py
@@ -1,1 +1,21 @@
 # Write your code below:
+filename = input("Enter the file name: ")
+
+print("\nPayroll Report")
+print("{:<15} {:<15} {:<15}".format("Last Name:",
+            "Hours Worked:",
+            "Wages Paid:"))
+
+with open(filename, "r") as file:
+    for line in file:
+        data = line.split()
+
+        last_name = data[0]
+        hourly_wage = float(data[1])
+        hours_worked = float(data[2])
+
+        wages_paid = hourly_wage * hours_worked
+
+        print("{:<15} {:<15.2f} ${:<14.2f}".format( last_name,
+            hours_worked,
+            wages_paid))
