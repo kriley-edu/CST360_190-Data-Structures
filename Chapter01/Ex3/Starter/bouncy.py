6e2ebde1a1ceeeefe04c06d63e6bb76e7b454f09
kriley-edu <kriley97@gmail.com>
2026-05-11 10:58:36 -0400

    Update bouncy.py

--- Chapter01/Ex3/Starter/bouncy.py
+++ Chapter01/Ex3/Starter/bouncy.py
@@ -1,1 +1,22 @@
 # Write your code below:
+def main():
+    height = float(input("Enter initial height: "))
+    bounces = int(input("Enter number of bounces: "))
+    index = float(input("Enter bounciness index: "))
+
+    down = height
+    up = down * index
+    total_distance = 0
+
+    for i in range(bounces):
+        # Calulate total distance
+        total_distance += (down + up)
+
+        # prepare next bounce height
+        down = up
+        up = down * index        
+
+    print(f"\nTotal distance traveled: {total_distance:.2f} feet")
+
+if __name__ == "__main__":
+    main()
