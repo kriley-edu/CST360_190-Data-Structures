8d0288189dc05d7efde80dac7c25dc2f49713bbf
kriley-edu <kriley97@gmail.com>
2026-05-14 07:25:58 -0400

    Update student.py

--- Chapter01/Ex10/Starter/student.py
+++ Chapter01/Ex10/Starter/student.py
@@ -1,5 +1,34 @@
 # Write your code below:
+import random
 
+class Student:
+    def __init__(self, name, numScores):
+        self._name = name
+        self._scores = [0] * numScores
+
+    def getName(self):
+        return self._name
+
+    def getScore(self, index):
+        return self._scores[index]
+
+    def setScore(self, index, value):
+        self._scores[index] = value
+
+    def getNumScores(self):
+        return len(self._scores)
+
+    def getHighScore(self):
+        return max(self._scores)
+
+    def getAverage(self):
+        return sum(self._scores) / len(self._scores)
+
+    def __str__(self):
+        result = f"Name: {self._name}\n"
+        for i, score in enumerate(self._scores, start=1):
+            result += f"Score {i}: {score}\n"
+        return result.strip()
 
 def main(numScores = 3):
     """Tests sorting."""
